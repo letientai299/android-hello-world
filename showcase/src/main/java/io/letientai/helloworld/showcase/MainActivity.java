7403dbf454e0368cdcaca15a5ebea43e48adb4e9
Tai <letientai299@gmail.com>
2018-12-11 03:27:09 +0800

    Init project

---
+++ showcase/src/main/java/io/letientai/helloworld/showcase/MainActivity.java
@@ -0,0 +1,13 @@
+package io.letientai.helloworld.showcase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
